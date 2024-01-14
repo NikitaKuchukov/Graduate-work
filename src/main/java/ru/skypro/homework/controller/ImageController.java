9513dde3ec41745416430cb82212579cfe97b6b5
Nikita Kuchukov <n.kuchukov@goodt.me>
2024-01-14 17:43:10 +0700

    issues fixed

--- src/main/java/ru/skypro/homework/controller/ImageController.java
+++ src/main/java/ru/skypro/homework/controller/ImageController.java
@@ -1,11 +1,17 @@
 package ru.skypro.homework.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import ru.skypro.homework.dto.ads.AdDto;
 import ru.skypro.homework.service.impl.ImageServiceImpl;
 
 @RestController
@@ -16,9 +22,13 @@ public class ImageController {
 
     private final ImageServiceImpl imageService;
 
-    /**
-     * Отображение картинок на странице
-     */
+    @Operation(summary = "Добавление объявления", tags = {"Картинки"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ok",
+                    content = @Content(schema = @Schema(implementation = String.class))),
+            @ApiResponse(responseCode = "401", description = "Unauthorized",
+                    content = @Content(schema = @Schema(implementation = String.class)))
+    })
     @GetMapping("image/{id}")
     private ResponseEntity<?> getImageById(@PathVariable int id) {
 
